72be782a77d90122debe537e7c7049fa6dec700f
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-09-27 14:33:39 -0700

    Codemod to update test format

--- tests/acceptance/load-shapes-test.js
+++ tests/acceptance/load-shapes-test.js
@@ -1,68 +1,63 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { currentURL, visit, findAll } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 
-moduleForAcceptance('Acceptance | Load Shapes');
+module('Acceptance | Load Shapes', function(hooks) {
+  setupApplicationTest(hooks);
 
-function testLoad(assert, selector) {
-  const done = assert.async(1);
-  assert.equal(currentURL(), '/');
-  const images = find(selector);
-  assert.equal(images.length, 1);
-  const img = images.eq(0);
+  function testLoad(assert, selector) {
+    const done = assert.async(1);
+    assert.equal(currentURL(), '/');
+    const images = findAll(selector);
+    assert.equal(images.length, 1);
+    const img = images[0];
 
-  const reloadedImage = new Image();
-  reloadedImage.addEventListener('error', () => {
-    const filename = img.attr('src').replace(/^.*[\\/]/, '')
-    assert.ok(false, `${filename} image didn't load`);
-    reloadedImage.remove();
-    done();
-  });
-  reloadedImage.addEventListener('load', () => {
-    assert.ok(true);
-    done();
-  });
-  reloadedImage.src =img.attr('src');
-}
+    const reloadedImage = new Image();
+    reloadedImage.addEventListener('error', () => {
+      const filename = img.src.replace(/^.*[\\/]/, '')
+      assert.ok(false, `${filename} image didn't load`);
+      reloadedImage.remove();
+      done();
+    });
+    reloadedImage.addEventListener('load', () => {
+      assert.ok(true);
+      done();
+    });
+    reloadedImage.src =img.src;
+  }
 
-test('circle with green background', function (assert) {
-  assert.expect(3);
-  assert.timeout(1000);
-  visit('/');
+  test('circle with green background', async function(assert) {
+    assert.expect(3);
+    assert.timeout(1000);
+    await visit('/');
 
-  andThen(function () {
     testLoad(assert, '.circle-with-green-background img');
   });
-});
 
-test('sized squares', function (assert) {
-  assert.expect(12);
-  assert.timeout(1000);
-  visit('/');
+  test('sized squares', async function(assert) {
+    assert.expect(12);
+    assert.timeout(1000);
+    await visit('/');
 
-  andThen(function () {
-    testLoad(assert, '.squares img:eq(0)');
-    testLoad(assert, '.squares img:eq(1)');
-    testLoad(assert, '.squares img:eq(2)');
-    testLoad(assert, '.squares img:eq(3)');
+    testLoad(assert, '.squares img:nth-of-type(1)');
+    testLoad(assert, '.squares img:nth-of-type(2)');
+    testLoad(assert, '.squares img:nth-of-type(3)');
+    testLoad(assert, '.squares img:nth-of-type(4)');
   });
-});
 
-test('transparent circle', function (assert) {
-  assert.expect(3);
-  assert.timeout(1000);
-  visit('/');
+  test('transparent circle', async function(assert) {
+    assert.expect(3);
+    assert.timeout(1000);
+    await visit('/');
 
-  andThen(function () {
     testLoad(assert, '.transparent-circle img');
   });
-});
 
-test('different destination square', function (assert) {
-  assert.expect(3);
-  assert.timeout(1000);
-  visit('/');
+  test('different destination square', async function(assert) {
+    assert.expect(3);
+    assert.timeout(1000);
+    await visit('/');
 
-  andThen(function () {
     testLoad(assert, '.square-in-a-different-destination img');
   });
 });
